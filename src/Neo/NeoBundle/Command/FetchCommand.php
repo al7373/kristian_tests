bb2fe707fc69d17d7d853f4fb2bc5a6a5a9c31e3
rra <rra@emachines.local>
2019-12-29 18:22:56 +0300

    displaying api data

--- src/Neo/NeoBundle/Command/FetchCommand.php
+++ src/Neo/NeoBundle/Command/FetchCommand.php
@@ -25,11 +25,58 @@ class FetchCommand extends Command
 
 		protected function execute(InputInterface $input, OutputInterface $output){
 			$since = $input->getOption('since');
+
+			$client = HttpClient::create();
+			$output->writeln(['retrieving data from nasa api']);
+			$response = $client->request(
+				'GET', 
+				'https://api.nasa.gov/neo/rest/v1/feed?detailed=true&api_key=0PwUQG3UbV278anbQuKzGOFpUKWuU2aQC8vFsXcE'
+			);
+
+			$statusCode = $response->getStatusCode();
+			if($statusCode == 200){
+				$output->writeln(['data retrieved']);
+			} else {
+				$output->writeln(['an error has occured']);
+				return;
+			}
+			$output->writeln(['displaying retrieved data']);
+
+			$content = $response->toArray();
+
 			$output->writeln([
 				'data since ' . $since . ' day(s) requested from nasa api',
 				'============',
 				'',
 			]);
+
+			$elementCount = $content["element_count"];
+			$nearEarthObjects = $content["near_earth_objects"];
+
+			foreach($nearEarthObjects as $date => $nearEarthObjectsPerDate){
+				foreach($nearEarthObjectsPerDate as $nearEarthObject){
+					$closeApproachData = $nearEarthObject["close_approach_data"];
+					$relativeVelocity = array_key_exists("relative_velocity", $closeApproachData) ? $closeApproachData["relative_velocity"] : false;
+					
+					$kilometersPerHour = $relativeVelocity ? $relativeVelocity["kilometers_per_hour"] : "unknown";
+
+					$isHazardous = $nearEarthObject["is_potentially_hazardous_asteroid"] ? "true" : "false";
+
+					$output->writeln([
+						"date: ".$date,
+						"reference: ".$nearEarthObject["neo_reference_id"],
+						"name: ".$nearEarthObject["name"],
+						"speed: ".$kilometersPerHour,
+						"is hazardous: ".$isHazardous,
+						"------------------------------------------------"	
+					]);
+				}
+			}
+
+			$output->writeln([
+				'count of Near-Earth Objects: ' . $elementCount
+			]);
+
 			return 0;
 		}
 }
